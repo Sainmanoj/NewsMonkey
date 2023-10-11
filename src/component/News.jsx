8fbeeb7962a2c7fe5c858a8e5aa11c2a911b4f8e
Manoj Kumar <103413680+Sainmanoj@users.noreply.github.com>
2023-10-11 14:59:20 +0530

    Update News.jsx

--- src/component/News.jsx
+++ src/component/News.jsx
@@ -15,8 +15,8 @@ export default class News extends Component {
 
     }
     updateNews = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=086e0ef84deb47419e8dbf9cb6b99f6c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-        let data = await fetch(url,{ 'mode': 'cors', headers: { 'Access-Control-Allow-Origin': 'http://localhost:5173' } });
+        let url = `http://newsapi.org/v2/top-headlines?country=in&category=${this.props.category}&apiKey=086e0ef84deb47419e8dbf9cb6b99f6c&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url,{ 'mode': 'cors', headers: { 'Access-Control-Allow-Origin': '*' } });
         let parseData = await data.json();
         console.log(parseData)
         this.setState({
